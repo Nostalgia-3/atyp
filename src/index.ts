024c9935d05d27e886261ec8f80cdf34e56bbb17
Nostalgia3 <legotronforce@gmail.com>
2024-02-26 11:28:45 -0800

    Got rid of tabs that might've gotten copied over

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,12 @@ import { Highlighter } from './highlighter.ts';
 import { DEFAULT_THEME } from './theme.ts'
 
 function ansiRegex({onlyFirst = false} = {}) {
-	const pattern = [
-		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
-	].join('|');
+    const pattern = [
+        '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+        '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
+    ].join('|');
 
-	return new RegExp(pattern, onlyFirst ? undefined : 'g');
+    return new RegExp(pattern, onlyFirst ? undefined : 'g');
 }
 
 type Theme = {
